2ca0264de761b1ec510d8873d29b7c6e20f5e64d
Cody Swann <cody@gunnertech.com>
2019-02-18 13:15:18 -0500

    HOTFIX - changes images on home page

--- react-client/src/Screens/Splash.js
+++ react-client/src/Screens/Splash.js
@@ -15,8 +15,8 @@ const Splash = props => {
   const { classes } = props;
   return (
     <div className={classes.root}>
-      <Typography paragraph>Simplisurvey is a tool for interpreting real time customer satisfaction and analysis.</Typography>
-      <Typography paragraph>It works by polling customers with a simple algorithm and presenting findings on an easy to use dashboard.</Typography>
+      <Typography paragraph>Simplisurvey is a tool for interpreting real-time customer satisfaction and analysis.</Typography>
+      <Typography paragraph>It works by polling customers with a simple algorithm and presenting findings on an easy-to-use dashboard.</Typography>
       <Typography paragraph>If you were invited to join, follow the link and use the email and/or phone number your invite was sent to, otherwise, get started with your own free organization: <Link to={`/dashboard`}>Sign In</Link></Typography>
       <Grid container className={classes.root} spacing={16} justify={'space-around'}>
         <Grid item xs={12} md={3} style={{marginTop: "48px"}}>
