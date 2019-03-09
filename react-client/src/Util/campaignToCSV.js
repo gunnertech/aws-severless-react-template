b5d30e821e8561e47789802b16a2fbeb10a96a85
Cody Swann <cody@gunnertech.com>
2019-03-08 19:01:48 -0500

    closes #13873

--- react-client/src/Util/campaignToCSV.js
+++ react-client/src/Util/campaignToCSV.js
@@ -2,43 +2,45 @@
 import { convertArrayToCSV } from "convert-array-to-csv";
 
 const campaignToCSV = (campaign, surveys, users) =>
-  Promise.resolve(["Campaign", "Survey", "Prompt", "Response", "Comments", 'Reviewer Name', 'Reviewer Email', 'Reviewed At', 'Reviewer Comments', "Sender's Name", "Sender's Email", "Respondent", "Date"])
+  Promise.resolve(["Campaign", "Survey", "Prompt", "Response", "Comments", 'Reviewer Name', 'Reviewer Email', 'Reviewed At (UTC -5)', 'Reviewer Comments', "Sender's Name", "Sender's Email", "Respondent", "Date (UTC -5)"])
     .then(header => ({
       header,
       data: surveys
         // .filter(survey => !!survey.responses.items[0])
-        .map(survey => ({
-          survey, 
-          selectedPrompt: survey.responses.items[0] && survey.surveyTemplate.prompts.items.find(prompt => 
-            prompt.options.items.find(option => 
-              option.id === survey.responses.items[0].optionId
-            )
-          ),
-          selectedOption: survey.responses.items[0] && survey.surveyTemplate.prompts.items.map(prompt => 
-            prompt.options.items.find(option => 
-              option.id === survey.responses.items[0].optionId
-            )
+        .map(survey => 
+          survey.surveyTemplate.prompts.items
+            .map(prompt => ({
+              prompt,
+              selectedOption: prompt.options.items.find(option => 
+                  !!survey.responses.items.find(response => option.id === response.optionId)
+                )
+            }))
+            .map(({prompt, selectedOption={}}) => console.log({prompt, selectedOption}) || ({
+              survey, 
+              prompt,
+              selectedOption,
+              response: survey.responses.items.find(response => 
+                response.optionId === selectedOption.id
+              )
+            }))
+            .map(({survey, prompt, selectedOption={}, response={}}) => console.log(survey, prompt, selectedOption, response) || ([
+              campaign.campaignTemplate.name, ///Campaign
+              survey.surveyTemplate.name,  //Survey
+              prompt.body, //Prompt
+              selectedOption.name, //Response
+              response.reason, ////Comments
+              (users.find(user => user.id === response.reviewerId) || {}).name, /// Review Name
+              (users.find(user => user.id === response.reviewerId) || {}).email, /// Review Email
+              response.reviewedAt, /// Reviewed At
+              response.reviewComment, /// Reviewer Comments
+              users.find(user => user.id === survey.userId).name, //Sender's Name
+              users.find(user => user.id === survey.userId).email, ///Sender's Email
+              `${survey.recipientContact} - ${survey.recipientIdentifier}`, ///Respondent
+              survey.createdAt //Date
+            ])
           )
-          .filter(option => !!option)[0],
-          response: survey.responses.items[0]
-        }))
-        .map(({survey, selectedPrompt={}, selectedOption={}, response={}}) => (
-        [
-          campaign.campaignTemplate.name, ///Campaign
-          survey.surveyTemplate.name,  //Survey
-          selectedPrompt.body, //Prompt
-          selectedOption.name, //Response
-          response.reason, ////Comments
-          (users.find(user => user.id === response.reviewerId) || {}).name, /// Review Name
-          (users.find(user => user.id === response.reviewerId) || {}).email, /// Review Email
-          response.reviewedAt, /// Reviewed At
-          response.reviewComment, /// Reviewer Comments
-          users.find(user => user.id === survey.userId).name, //Sender's Name
-          users.find(user => user.id === survey.userId).email, ///Sender's Email
-          `${survey.recipientContact} - ${survey.recipientIdentifier}`, ///Respondent
-          survey.createdAt //Date
-        ]
-      ))
+      )
+      .flat()
     })
   )
   .then(({header, data}) => 
