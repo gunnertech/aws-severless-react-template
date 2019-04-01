258c0afb3b0c9a16360f4f8b193408a500689d1c
Cody Swann <cody@gunnertech.com>
2019-04-01 16:25:29 -0400

    hotfix

--- react-client/src/Util/campaignToCSV.js
+++ react-client/src/Util/campaignToCSV.js
@@ -1,6 +1,41 @@
 
 import { convertArrayToCSV } from "convert-array-to-csv";
 
+if (!Array.prototype.flat) {
+	Object.defineProperties(Array.prototype, {
+		flat: {
+			configurable: true,
+			value: function flat() {
+				let depth = isNaN(arguments[0]) ? 1 : Number(arguments[0]);
+				const stack = Array.prototype.slice.call(this);
+				const result = [];
+
+				while (depth && stack.length) {
+					const next = stack.pop();
+
+					if (Object(next) instanceof Array) {
+						--depth;
+
+						Array.prototype.push.apply(stack, next);
+					} else {
+						result.unshift(next);
+					}
+				}
+
+				return result.concat(stack);
+			},
+			writable: true
+		},
+		flatMap: {
+			configurable: true,
+			value: function flatMap(callback) {
+				return Array.prototype.map.apply(this, arguments).flat();
+			},
+			writable: true
+		}
+	});
+}
+
 const campaignToCSV = (campaign, surveys, users) =>
   Promise.resolve(["Campaign", "Survey", "Prompt", "Response", "Comments", 'Reviewer Name', 'Reviewer Email', 'Reviewed At (UTC -5)', 'Reviewer Comments', "Sender's Name", "Sender's Email", "Respondent", "Date (UTC -5)"])
     .then(header => ({
