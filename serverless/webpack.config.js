53cafd22489eeeadf33d489299d4fe3332336509
Cody Swann <cody@gunnertech.com>
2019-06-09 20:32:07 -0400

    minor

--- serverless/webpack.config.js
+++ serverless/webpack.config.js
@@ -1,21 +1,32 @@
-const path = require('path');
-// eslint-disable-next-line import/no-unresolved
 const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
-  module: {
-    rules: [{
-      test: /\.js$/,
-      loaders: ['babel-loader'],
-      include: __dirname,
-      exclude: /node_modules/,
-    }],
+  // Generate sourcemaps for proper error messages
+  devtool: 'source-map',
+  // Since 'aws-sdk' is not compatible with webpack,
+  // we exclude all node dependencies
+  externals: [nodeExternals()],
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  optimization: {
+    // We no not want to minimize our code.
+    minimize: false,
   },
-  output: {
-    libraryTarget: 'commonjs',
-    path: path.join(__dirname, '.webpack'),
-    filename: '[name].js',
+  performance: {
+    // Turn off size warnings for entry points
+    hints: false,
+  },
+  // Run babel on all .js files and skip those in node_modules
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: __dirname,
+        exclude: /node_modules/,
+      },
+    ],
   },
 };
