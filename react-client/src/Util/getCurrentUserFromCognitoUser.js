5f6385a8c20c941599a1862fb4d35915a993d034
Cody Swann <cody@gunnertech.com>
2019-06-13 20:05:20 -0400

    minor

--- react-client/src/Util/getCurrentUserFromCognitoUser.js
+++ react-client/src/Util/getCurrentUserFromCognitoUser.js
@@ -1,6 +1,6 @@
 /// ADD LOGIC TO CREATE USERS, SEND INVITATIONS, ADD TO GROUPS, ETC WITH APPSYNC HERE. JUST REMEMBER TO RETURN A USER OBJECT
 
-const getCurrentUserFromCognitoUser = (cognitoUser, {groupName}) =>
+const getCurrentUserFromCognitoUser = (apolloClient, cognitoUser, {groupName}) =>
   Promise.resolve(cognitoUser)
 
 export default getCurrentUserFromCognitoUser;
