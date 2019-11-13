3b06d3e86459473b4ebde39e135c2ed8beee92aa
Cody Swann <cody@gunnertech.com>
2019-11-12 22:03:32 -0500

    minor

--- serverless/amplify/config.js
+++ serverless/amplify/config.js
@@ -5,7 +5,7 @@ module.exports.env = () => {
   const contents = fs.readFileSync('../amplify/src/aws-exports.js', 'utf8').split(/\r|\n/)
     .map(line => line.trim())
     .filter(line =>
-      !!line.startsWith('"') && !!line.match(/:/)
+      !!line.startsWith('"') && !!line.match(/:/) && !line.match(/\}/) && !line.match(/\{/) && !line.match(/\]/) && !line.match(/\[/)
     )
     .map(line => line.replace(/,$/, ""))
     .join(",")
