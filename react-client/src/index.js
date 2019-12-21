039054ddc6ec27c892111a3c97cb19d416f25ccd
Cody Swann <cody@gunnertech.com>
2019-12-21 16:43:25 -0500

    minor

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -9,4 +9,9 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register({
+  onUpdate: () => {
+    window.alert("A new version of this app is available. Please close any other tabs running this app and refresh this page.");
+    window.location.reload();
+  }
+})
