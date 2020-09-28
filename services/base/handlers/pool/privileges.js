3ed7b57068b317b4761f87388ca646a548430ae1
Cody Swann <cody@gunnertech.com>
2020-09-28 12:09:29 -0400

    Minor  [skip-cd]

--- services/base/handlers/pool/privileges.js
+++
@@ -1,8 +0,0 @@
-import { appsync } from '../clients';
-
-// import { accept } from '.';
-
-
-// eslint-disable-next-line import/prefer-default-export
-export const js = async event => 
-  Promise.resolve({stub: true})
